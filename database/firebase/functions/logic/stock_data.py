f81b184c7bf56822db243de817b72fa95342c674
Randy Panopio <randypanopio@gmail.com>
2024-02-21 23:05:05 -0800

    Merge Database branch to main (#10)
    * cloud funcs
    
    * create admin script for db population
    
    * first attempt of update func
    
    * first pass of update db cloud func
    
    * add fault tolerant utils
    
    * deploy autoupdate stocks db cloud func

---
+++ database/firebase/functions/logic/stock_data.py
@@ -0,0 +1,31 @@
+import yfinance as yf
+
+def get_data(ticker: str, start: str, end: str):
+    '''
+        Returns json object of historic data of passed ticker
+
+        start:
+            datetime string
+        end:
+            datetime string
+    '''
+    ticker = ticker.upper()
+    # TODO add validation but dunno how it works really
+    
+    stock_data = yf.download(ticker, start=start, end=end)
+    stock_data_entries = []
+    for date, row in stock_data.iterrows():
+        date_str = date.strftime("%Y-%m-%d")
+        entry = {
+            "date": date_str,
+            "data": {
+                "Open": row["Open"],
+                "High": row["High"],
+                "Low": row["Low"],
+                "Close": row["Close"],
+                "Adj Close": row["Adj Close"],
+                "Volume": row["Volume"]
+            }
+        }
+        stock_data_entries.append(entry)
+    return stock_data_entries
---
+++ database/setup.md
@@ -0,0 +1,16 @@
+
+https://firebase.google.com/docs/functions/get-started?gen=2nd#python_1
+install firebase tools
+npm install -g firebase-tools
+
+run firebase login 
+
+
+-- functiosn
+
+install venv in 
+firebase/functions
+run pip install requirements.txt
+
+create the super_secrets directory under functions
+add the serviceAccKey.json (request admin)
