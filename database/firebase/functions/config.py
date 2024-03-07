64d098e5410f524643a94cc6dda135c5f680e498
Randy Panopio <randypanopio@gmail.com>
2024-03-06 21:09:05 -0800

    update data model and cloud func for ticker data [skip-all]

---
+++ database/firebase/functions/config.py
@@ -0,0 +1,10 @@
+"""
+    Config and constants
+"""
+from datetime import datetime
+DATE_FORMAT = '%Y-%m-%d'
+EPOCH_DATE = '1970-01-01' # UNIX time
+EPOCH_DATETIME = datetime.strptime(EPOCH_DATE, DATE_FORMAT)
+
+TICKER_COLLECTION = 'ticker_data'
+SERIES_COLLECTION = 'entries' #generic name for time series collection
