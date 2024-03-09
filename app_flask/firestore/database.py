41d73c61ae9f63533f06c05ef07f13273261cb93
Randy Panopio <randypanopio@gmail.com>
2024-03-08 18:52:52 -0800

    i dont think this works

--- app_flask/firestore/database.py
+++ app_flask/firestore/database.py
@@ -29,10 +29,9 @@ class DatabaseManager:
         sv_path = "super_secrets/serviceAccKey.json"
         if os.path.exists(sv_path):
             os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = sv_path
-            initialize_app(credentials.Certificate(sv_path))
         else:
-            env = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
-            initialize_app(credentials.Certificate(env))
+            os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS") # type: ignore
+        initialize_app()
         self.db = firestore.client()
 
     def __new__(cls):
